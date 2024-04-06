3fdddc8e0380eed60497e6c910e56fbf140874bb
Aryaman Rishabh <55853813+aryamanrishabh@users.noreply.github.com>
2024-04-06 18:24:20 -0400

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,12 @@ const Navbar: React.FC = () => {
     <div className="flex w-full items-center gap-x-8 bg-gradient-to-b from-blue-300 to-indigo-400 px-8 py-4 font-mono text-white lg:gap-x-16">
       <Link href="/">
         <span className="cursor-pointer text-4xl font-bold tracking-wider">
-          ih
+          ChromaIcons
         </span>
       </Link>
       <Link href="/assets">
         <span className="cursor-pointer text-lg font-semibold tracking-wide">
-          ASSets
+          Assets
         </span>
       </Link>
     </div>
